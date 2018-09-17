cce50861d7bb46b01478be9eab656adc191d15e9
Chris Eddy <ceddy.zen@gmail.com>
2018-09-16 22:58:50 -0700

    remove more camera stuff for VR

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ document.body.appendChild(renderer.domElement);
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  // camera.aspect = window.innerWidth / window.innerHeight;
+  // camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
