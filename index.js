108be49fb61197f7d2afe9050104f6d932d54c62
Chris Eddy <ceddy.zen@gmail.com>
2018-09-18 00:02:20 -0700

    increase render distance

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Distances are 1:100,000
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 100000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 150000);
 
 const renderer = new THREE.WebGLRenderer({antialias: true, powerPreference: 'high-performance'});
 renderer.setSize( window.innerWidth, window.innerHeight );
