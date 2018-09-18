9c15197e8f7683ecfab483db22eb29cb7c7569cd
Chris Eddy <ceddy.zen@gmail.com>
2018-09-18 03:16:01 -0700

    comment out audio

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ window.onload = function() {
 
   musicController.onChange(function(value) {
     if(value){
-      backgroundMusic.play();
+      // backgroundMusic.play();
     }
     else{
-      backgroundMusic.pause();
+      // backgroundMusic.pause();
     }
   });
 
@@ -87,48 +87,48 @@ window.onload = function() {
 
 };
 
-// Music
-
-// instantiate a listener
-const audioListener = new THREE.AudioListener();
-
-// add the listener to the camera
-camera.add(audioListener);
-
-// instantiate audio object
-const backgroundMusic = new THREE.Audio(audioListener);
-
-// add the audio object to the scene
-scene.add(backgroundMusic);
-
-// instantiate a loader
-const loader = new THREE.AudioLoader();
-
-// load a resource
-loader.load(
-  // resource URL
-  'ＮＯＳＴＡＬＧＩＣ.mp3',
-
-  // onLoad callback
-  function ( audioBuffer ) {
-    // set the audio object buffer to the loaded object
-    backgroundMusic.setBuffer( audioBuffer );
-
-    // play the audio
-    backgroundMusic.play();
-  },
-
-  // onProgress callback
-  function ( xhr ) {
-    console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-  },
-
-  // onError callback
-  function ( err ) {
-    console.log( 'An error happened' );
-    console(err.stack)
-  }
-);
+// // Music
+//
+// // instantiate a listener
+// const audioListener = new THREE.AudioListener();
+//
+// // add the listener to the camera
+// camera.add(audioListener);
+//
+// // instantiate audio object
+// const backgroundMusic = new THREE.Audio(audioListener);
+//
+// // add the audio object to the scene
+// scene.add(backgroundMusic);
+//
+// // instantiate a loader
+// const loader = new THREE.AudioLoader();
+//
+// // load a resource
+// loader.load(
+//   // resource URL
+//   'ＮＯＳＴＡＬＧＩＣ.mp3',
+//
+//   // onLoad callback
+//   function ( audioBuffer ) {
+//     // set the audio object buffer to the loaded object
+//     backgroundMusic.setBuffer( audioBuffer );
+//
+//     // play the audio
+//     backgroundMusic.play();
+//   },
+//
+//   // onProgress callback
+//   function ( xhr ) {
+//     console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+//   },
+//
+//   // onError callback
+//   function ( err ) {
+//     console.log( 'An error happened' );
+//     console(err.stack)
+//   }
+// );
 
 // create starbox
 
