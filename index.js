a2df0ee4f3dc99df596c67a880cc6c08fc48bf40
Chris Eddy <ceddy.zen@gmail.com>
2018-09-17 23:45:05 -0700

    ＮＯＳＴＡＬＧＩＣ.mp3

--- index.js
+++ index.js
@@ -17,6 +17,49 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+// Music
+
+// instantiate a listener
+const audioListener = new THREE.AudioListener();
+
+// add the listener to the camera
+camera.add(audioListener);
+
+// instantiate audio object
+const backgroundMusic = new THREE.Audio(audioListener);
+
+// add the audio object to the scene
+scene.add(backgroundMusic);
+
+// instantiate a loader
+const loader = new THREE.AudioLoader();
+
+// load a resource
+loader.load(
+  // resource URL
+  'ＮＯＳＴＡＬＧＩＣ.mp3',
+
+  // onLoad callback
+  function ( audioBuffer ) {
+    // set the audio object buffer to the loaded object
+    backgroundMusic.setBuffer( audioBuffer );
+
+    // play the audio
+    backgroundMusic.play();
+  },
+
+  // onProgress callback
+  function ( xhr ) {
+    console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+  },
+
+  // onError callback
+  function ( err ) {
+    console.log( 'An error happened' );
+    console(err.stack)
+  }
+);
+
 // create starbox
 
 const imagePrefix = "images/2kstars/";
