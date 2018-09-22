0ffac81014ec76a8dc62a8b2b0d6cfd4b7392069
flexo <f37314b1@opayq.com>
2018-09-22 15:11:50 -0400

    Update GamepadUtility.js

--- js/input/GamepadUtility.js
+++ js/input/GamepadUtility.js
@@ -2,9 +2,41 @@
  * Gamepad Utility
  */
 
+// Gamepad button index map for Oculus Rift, Go and Gear VR
+const btnMap = {
+    stick:   0,
+    trigger: 1,
+}
+
 GamepadUtility = class {
     constructor() {
         this.gamepads = [];
+        this.gamepad = {
+            left:  {
+                trigger: {
+                    press: () => {return this.buttonState("left", btnMap.trigger, "press")},
+                    touch: () => {return this.buttonState("left", btnMap.trigger, "touch")},
+                    value: () => {return this.buttonState("left", btnMap.trigger, "value")}
+                },
+                stick: {
+                    press: () => {return this.buttonState("left", btnMap.stick, "press")},
+                    touch: () => {return this.buttonState("left", btnMap.stick, "touch")},
+                    value: () => {return this.buttonState("left", btnMap.stick, "value")}
+                }
+            },
+            right: {
+                trigger: {
+                    press: () => {return this.buttonState("right", btnMap.trigger, "press")},
+                    touch: () => {return this.buttonState("right", btnMap.trigger, "touch")},
+                    value: () => {return this.buttonState("right", btnMap.trigger, "value")}
+                },
+                stick: {
+                    press: () => {return this.buttonState("right", btnMap.stick, "press")},
+                    touch: () => {return this.buttonState("right", btnMap.stick, "touch")},
+                    value: () => {return this.buttonState("right", btnMap.stick, "value")}
+                }
+            }
+        }
         this.setup();
     }
 
@@ -20,12 +52,12 @@ GamepadUtility = class {
 
     update() {
         if(this.checkGamepads())
-            Array.prototype.forEach.call(navigator.getGamepads(), (pad, pIndex) => {
+            Array.prototype.forEach.call(navigator.getGamepads(), (pad, padIndex) => {
                 if(pad) {
-                    pad.buttons.forEach((btn, bIndex) => {
-                        this.gamepads[pIndex].buttons[bIndex].pressed = btn.pressed;
-                        this.gamepads[pIndex].buttons[bIndex].touched = btn.touched;
-                        this.gamepads[pIndex].buttons[bIndex].value   = btn.value;
+                    pad.buttons.forEach((btn, btnIndex) => {
+                        this.gamepads[padIndex].buttons[btnIndex].pressed = btn.pressed;
+                        this.gamepads[padIndex].buttons[btnIndex].touched = btn.touched;
+                        this.gamepads[padIndex].buttons[btnIndex].value   = btn.value;
                     });
                 }
             });
@@ -36,19 +68,21 @@ GamepadUtility = class {
         return this.gamepads.length > 0 ? true : false;
     }
 
-    buttonPress(name) {
+    buttonState(hand, btnIndex, action) {
         if(this.checkGamepads())
-            switch(name) {
-                case "leftTrigger":
+            switch(action) {
+                case "press":
                     for(let pad of this.gamepads)
-                        if(pad.hand === "left") return pad.buttons[1].pressed;
+                        if(pad.hand === hand) return pad.buttons[btnIndex].pressed;
                     return false;
-                case "rightTrigger":
+                case "touch":
                     for(let pad of this.gamepads)
-                        if(pad.hand === "right") return pad.buttons[1].pressed;
+                        if(pad.hand === hand) return pad.buttons[btnIndex].touched;
                     return false;
-                default:
-                    return false;
+                case "value":
+                    for(let pad of this.gamepads)
+                        if(pad.hand === hand) return pad.buttons[btnIndex].value;
+                    return 0;
             }
         else return false;
     }
